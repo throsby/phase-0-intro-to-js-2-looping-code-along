4486168ec214a2b12f8c6baff048edf35a218d6b
Throsby Wells <throsbywells@gmail.com>
2022-03-17 00:00:31 -0400

    commit tree 74ec279a006caa26b930ef19c87609a736b45aa1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names,message){
+    let arr = []
+    for (let i=0; i< names.length;i++){
+        arr.push("Thank you, "+names[i]+", for the wonderful "+message+" gift!")
+    }
+    return arr
+}
+
+
+
+function countDown(num) {
+    for (let i=num; i >= 0; i-- ){
+        console.log(i)
+    }
+}
